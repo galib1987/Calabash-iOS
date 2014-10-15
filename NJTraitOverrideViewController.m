e96f59065018554e7f8992a7a8a73d023581453b
Amos Elmaliah <amosel@gmail.com>
2014-10-15 14:20:10 +0300

    Basic iPad layout controller in storyboard

---
+++ NJTraitOverrideViewController.m
@@ -0,0 +1,21 @@
+//
+//  TraitOverrideViewController.m
+//  TailWinder
+//
+//  Created by Amos Elmaliah on 10/15/14.
+//  Copyright (c) 2014 Amos Elmaliah. All rights reserved.
+//
+
+#import "NJTraitOverrideViewController.h"
+
+@interface NJTraitOverrideViewController ()
+
+@end
+
+@implementation NJTraitOverrideViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
