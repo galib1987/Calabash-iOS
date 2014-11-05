cc7f8f616665bb692c528d9518e0deb45024a218
Amos Elmaliah <amosel@gmail.com>
2014-11-05 13:00:07 +0200

    iPad navigation title resize fixes and refactoring.

--- Tailwind/MiddleLevelUI/NJOPNavigationTitleView.m
+++ Tailwind/MiddleLevelUI/NJOPNavigationTitleView.m
@@ -7,30 +7,15 @@
 //
 
 #import "NJOPNavigationTitleView.h"
-#import "NJOPCollectionViewFlowLayout.h"
 
 @implementation NJOPNavigationTitleView
 
 -(CGSize)sizeThatFits:(CGSize)size {
 	CGSize fittedSize = [super sizeThatFits:size];
-	fittedSize.width = size.width;
-	return fittedSize;
-}
-
-- (void)applyLayoutAttributes:(NJOPCollectionViewFlowLayoutAttributes *)layoutAttributes {
-
-	[UIView beginAnimations:@"" context:nil];
-
-	if (layoutAttributes.progressiveness >= 0.58) {
-		self.leftTItle.alpha = 1;
-		self.rightTitle.alpha = 1;
-
-	} else {
-		self.leftTItle.alpha = 0;
-		self.rightTitle.alpha = 0;
+	if (self.fittedSizeForSize) {
+		return self.fittedSizeForSize(size,fittedSize);
 	}
-
-	[UIView commitAnimations];
+	return fittedSize;
 }
 
 @end
