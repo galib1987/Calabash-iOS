313e1e89c47519d297e26c4b452eff68fae5459f
Amos Elmaliah <amosel@gmail.com>
2014-11-06 10:18:56 +0200

    iPad- updated Layout section insets.(+cleanups)

--- Tailwind/MiddleLevelUI/NJOPBriefTopCollectionViewCell.m
+++ Tailwind/MiddleLevelUI/NJOPBriefTopCollectionViewCell.m
@@ -9,28 +9,4 @@
 #import "NJOPBriefTopCollectionViewCell.h"
 
 @implementation NJOPBriefTopCollectionViewCell
-
--(void)layoutSubviews {
-	[super layoutSubviews];
-//	[self.subviews enumerateObjectsUsingBlock:^(UIView* subview, NSUInteger idx, BOOL *stop) {
-//		subview.layer.borderWidth = 1.0;
-//		subview.layer.borderColor = [UIColor greenColor].CGColor;
-//	}];
-}
-//-(void)applyLayoutAttributes:(UICollectionViewLayoutAttributes *)layoutAttributes {
-//
-//}
-//-(UICollectionViewLayoutAttributes *)preferredLayoutAttributesFittingAttributes:(UICollectionViewLayoutAttributes *)layoutAttributes {
-//
-//	CGRect frame = layoutAttributes.frame;
-//	CGFloat columnWidth = [(UICollectionView*)self.superview contentSize].width;
-//	CGSize fittingSize = CGSizeMake(columnWidth, UILayoutFittingExpandedSize.height);
-//	frame.size = fittingSize;
-//	layoutAttributes.frame = frame;
-//	return layoutAttributes;
-//
-////	UICollectionViewLayoutAttributes * atts = [super preferredLayoutAttributesFittingAttributes:layoutAttributes];
-////	return atts;
-//}
-
 @end
--- Tailwind/MiddleLevelUI/NJOPParentViewController.m
+++ Tailwind/MiddleLevelUI/NJOPParentViewController.m
@@ -8,14 +8,66 @@
 
 #import "NJOPParentViewController.h"
 #import "NJOPNavigationTitleView.h"
+#import "NJOPActionsViewController.h"
+#import "NJOPReservationCollectionViewController.h"
+#import "NJOPCollectionViewFlowLayout.h"
 
 @interface NJOPParentViewController ()
+-(NJOPReservationCollectionViewController*)reservationController;
+-(NJOPActionsViewController*)actionsViewController;
+@end
+
+@interface NSObject (FindBy)
++(instancetype)njop_first:(NSArray*)array;
+@end
+
+@implementation NSObject (FindBy)
+
+id findByClass(NSArray*array,Class class) {
+	__block typeof(Class) objectOfThisClass;
+	[array enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+		if ([obj isKindOfClass:class]) {
+			objectOfThisClass = obj;
+		}
+	}];
+	return objectOfThisClass;
+
+}
+
++(instancetype)njop_first:(NSArray *)array {
+	return findByClass(array, self);
+}
+
 @end
 
 @implementation NJOPParentViewController
 
+-(NJOPReservationCollectionViewController *)reservationController {
+	return [NJOPReservationCollectionViewController njop_first:self.childViewControllers];
+}
+
+-(NJOPActionsViewController *)actionsViewController {
+	return [NJOPActionsViewController njop_first:self.childViewControllers];
+}
+
 - (void)viewDidLoad {
 	[super viewDidLoad];
+
+	NJOPReservationCollectionViewController* reservationController = [self reservationController];
+	if (reservationController) {
+		NJOPCollectionViewFlowLayout*layout = (NJOPCollectionViewFlowLayout*)reservationController.collectionView.collectionViewLayout;
+		NSAssert([layout isKindOfClass:[NJOPCollectionViewFlowLayout class]], @"");
+		layout.sectionInset = UIEdgeInsetsMake(8, 8, 8, 8);
+	}
+	NJOPActionsViewController* actionController = [self actionsViewController];
+	if (actionController) {
+		NJOPCollectionViewFlowLayout*layout = (NJOPCollectionViewFlowLayout*)actionController.collectionView.collectionViewLayout;
+		NSAssert([layout isKindOfClass:[NJOPCollectionViewFlowLayout class]], @"");
+		layout.sectionInset = UIEdgeInsetsMake(8, 8, 8, 8);
+	}
+
+
+
 	self.view.backgroundColor = SCROLLVIEW_BACKGORUND_COLOR;
 	UINib* nib = [UINib nibWithNibName:@"NJOPNavigationTitleView"
 															bundle:nil];
