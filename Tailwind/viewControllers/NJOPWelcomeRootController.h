8a938382918d647d947b42e09c4e2209bcda6535
Angus Lo <angus.lo@havasww.com>
2014-12-22 17:43:40 -0500

    Added welcome scene

---
+++ Tailwind/viewControllers/NJOPWelcomeRootController.h
@@ -0,0 +1,19 @@
+//
+//  NJOPWelcomeRootController.h
+//  Tailwind
+//
+//  Created by Angus.Lo on 12/22/14.
+//  Copyright (c) 2014 NetJets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NJOPWelcomeContentController.h"
+
+@interface NJOPWelcomeRootController : UIViewController <UIPageViewControllerDataSource>
+- (IBAction)skipButton:(id)sender;
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+@property (strong, nonatomic) NSArray *pageBgs;
+@property (strong, nonatomic) NSArray *pageHeaders;
+@property (strong, nonatomic) NSArray *pageDescs;
+
+@end
