91a3dde2ae4be58a4a4f469f03aa966e2c3cbc83
Rosa McGee <rosajmcgee@gmail.com>
2014-12-23 14:48:19 -0500

    Added Account scene with Past Flights table.

---
+++ Tailwind/viewControllers/NJOPFlightTableCell.h
@@ -0,0 +1,13 @@
+//
+//  NJOPFlightTableCell.h
+//  Tailwind
+//
+//  Created by netjets on 12/23/14.
+//  Copyright (c) 2014 NetJets. All rights reserved.
+//
+
+#import "NJOPTableViewCell.h"
+
+@interface NJOPFlightTableCell : NJOPTableViewCell
+
+@end
