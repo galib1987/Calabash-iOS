2f6a758e1681a242fc802d47a448511cabf35967
Angus Lo <angus.lo@havasww.com>
2014-12-23 14:52:56 -0500

    Welcome scene: added cutout peephole

---
+++ Tailwind/viewControllers/DrawShapes/NJOPDrawRectWithCircleCutout.h
@@ -0,0 +1,13 @@
+//
+//  NJOPDrawRectWithCircleCutout.h
+//  Tailwind
+//
+//  Created by Angus.Lo on 12/23/14.
+//  Copyright (c) 2014 NetJets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NJOPDrawRectWithCircleCutout : UIView
+
+@end
