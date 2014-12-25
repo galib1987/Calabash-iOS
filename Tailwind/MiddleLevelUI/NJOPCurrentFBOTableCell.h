b2360379c2f94c2084ad65d2ec64cf88abc24b23
Rosa McGee <rosajmcgee@gmail.com>
2014-12-25 02:27:14 -0500

    Connected some outlets for cells in Home View. Definitely needs better names going forward.

--- Tailwind/MiddleLevelUI/NJOPCurrentFBOTableCell.h
+++ Tailwind/MiddleLevelUI/NJOPCurrentFBOTableCell.h
@@ -10,4 +10,20 @@
 
 @interface NJOPCurrentFBOTableCell : NJOPTableViewCell
 
+@property (weak, nonatomic) IBOutlet UILabel *planeNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tailNumberLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *departureLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fromFBOTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fromFBOAirportCodeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fromFBOAirportNameLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *arrivalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *toFBOTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *toFBOAirportCodeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *toFBOAirportNameLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *travelTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *layoverLabel;
+
 @end
