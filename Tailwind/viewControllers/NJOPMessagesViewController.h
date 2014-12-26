b53e3a6620dc5ac9e75c2be10bc7497398b6a453
Rosa McGee <rosajmcgee@gmail.com>
2014-12-26 12:11:18 -0500

    Added view controller for Messages. Set storyboard ID.

---
+++ Tailwind/viewControllers/NJOPMessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPMessagesViewController.h
+//  Tailwind
+//
+//  Created by netjets on 12/26/14.
+//  Copyright (c) 2014 NetJets. All rights reserved.
+//
+
+#import "SimpleDataSourceTableViewController.h"
+
+@interface NJOPMessagesViewController : SimpleDataSourceTableViewController
+
+@end
