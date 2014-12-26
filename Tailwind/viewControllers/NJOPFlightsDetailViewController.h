b27b0a416b2f0a5c8bc974491b2d1781a51d55fb
Rosa McGee <rosajmcgee@gmail.com>
2014-12-26 12:07:36 -0500

    Added view controllers for Flights table and detail view. Set storyboard ID's.

---
+++ Tailwind/viewControllers/NJOPFlightsDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPFlightsDetailViewController.h
+//  Tailwind
+//
+//  Created by netjets on 12/26/14.
+//  Copyright (c) 2014 NetJets. All rights reserved.
+//
+
+#import "SimpleDataSourceTableViewController.h"
+
+@interface NJOPFlightsDetailViewController : SimpleDataSourceTableViewController
+
+@end
