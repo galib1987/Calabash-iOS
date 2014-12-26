a8cd40aabfdd55fe5812f3c2ae981aaae1a075b6
Rosa McGee <rosajmcgee@gmail.com>
2014-12-26 11:55:28 -0500

    Added controller for booking view. Set Storyboard ID.

---
+++ Tailwind/viewControllers/NJOPBookingViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPBookingViewController.h
+//  Tailwind
+//
+//  Created by netjets on 12/26/14.
+//  Copyright (c) 2014 NetJets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NJOPBookingViewController : UITableViewController
+
+@end
