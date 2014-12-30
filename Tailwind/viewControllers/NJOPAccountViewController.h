05250f53c09a3ab635519eab87ba6221982ff07a
Rosa McGee <rosajmcgee@gmail.com>
2014-12-30 09:38:13 -0500

    Remade Account storyboard due to corrupt file.

---
+++ Tailwind/viewControllers/NJOPAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPAccountViewController.h
+//  Tailwind
+//
+//  Created by netjets on 12/30/14.
+//  Copyright (c) 2014 NetJets. All rights reserved.
+//
+
+#import "SimpleDataSourceTableViewController.h"
+
+@interface NJOPAccountViewController : SimpleDataSourceTableViewController
+
+@end
