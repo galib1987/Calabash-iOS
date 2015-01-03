53b8f4b5dbe07a984ca8d80e589bfdc40f70a69f
Rosa McGee <rosajmcgee@gmail.com>
2015-01-03 16:02:29 -0500

    Added last cell - Trip Complete - from PSD.

---
+++ Tailwind/NJOPTripCompleteCell.h
@@ -0,0 +1,13 @@
+//
+//  NJOPTripCompleteCell.h
+//  Tailwind
+//
+//  Created by netjets on 1/3/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "NJOPTableViewCell.h"
+
+@interface NJOPTripCompleteCell : NJOPTableViewCell
+
+@end
