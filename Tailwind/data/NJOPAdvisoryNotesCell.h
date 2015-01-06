4af8995debac79848ebe3c5c69197af8a2c8dd5f
Rosa McGee <rosajmcgee@gmail.com>
2015-01-05 21:43:01 -0500

    Added advisory notes controller.

---
+++ Tailwind/data/NJOPAdvisoryNotesCell.h
@@ -0,0 +1,16 @@
+//
+//  NJOPAdvisoryNotesCell.h
+//  Tailwind
+//
+//  Created by netjets on 1/5/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "NJOPTableViewCell.h"
+
+@interface NJOPAdvisoryNotesCell : NJOPTableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *topLabel;
+@property (nonatomic, weak) IBOutlet UILabel *detailLabel;
+
+@end
---
+++ Tailwind/viewControllers/NJOPAdvisoryNotesController.m
@@ -0,0 +1,29 @@
+//
+//  NJOPAdvisoryNotesController.m
+//  Tailwind
+//
+//  Created by netjets on 1/5/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "NJOPAdvisoryNotesController.h"
+#import "NJOPAdvisoryNotesCell.h"
+
+@interface NJOPAdvisoryNotesController ()
+
+@end
+
+@implementation NJOPAdvisoryNotesController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
