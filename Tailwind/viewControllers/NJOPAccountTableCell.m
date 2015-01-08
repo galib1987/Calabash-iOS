a61ce11059e12f81f551573224f61a1d747a563e
Angus Lo <angus.lo@havasww.com>
2015-01-08 16:18:35 -0500

    booking scene: added select account scene

---
+++ Tailwind/viewControllers/NJOPAccountTableCell.m
@@ -0,0 +1,21 @@
+//
+//  NJOPAccountTableCell.m
+//  Tailwind
+//
+//  Created by netjets on 1/8/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "NJOPAccountTableCell.h"
+
+@implementation NJOPAccountTableCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+ */
+
+@end
---
+++ Tailwind/viewControllers/NJOPSelectAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPSelectAccountViewController.h
+//  Tailwind
+//
+//  Created by netjets on 1/8/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "SimpleDataSourceTableViewController.h"
+
+@interface NJOPSelectAccountViewController : SimpleDataSourceTableViewController
+
+@end
