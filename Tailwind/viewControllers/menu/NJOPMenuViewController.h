a473ac647a204156194420fd4e4a19b0a08ff303
David Lin <davidlin@Davids-MacBook-Air.local>
2015-01-08 03:38:05 -0500

    committing to prepare for conflict with Rosa's changes

---
+++ Tailwind/viewControllers/menu/NJOPMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPMenuViewController.h
+//  Tailwind
+//
+//  Created by David Lin on 1/8/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NJOPMenuViewController : UIViewController
+
+@end
