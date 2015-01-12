2f093997301f17faa4a343667ce19adefcafd6a1
Angus Lo <angus.lo@havasww.com>
2015-01-12 15:19:07 -0500

    booking scene: added picker view for aircraft selection

--- Tailwind/viewControllers/NJOPBookingViewController.m
+++ Tailwind/viewControllers/NJOPBookingViewController.m
@@ -28,6 +28,11 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.aircraftPicker = [[UIPickerView alloc] init];
+    [self.aircraftPicker setDataSource: self];
+    [self.aircraftPicker setDelegate: self];
+    self.aircraftInput.inputView = self.aircraftPicker;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -102,5 +107,16 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - Aircraft UIPicker
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    // STUB
+    return 5;
+}
 
 @end
