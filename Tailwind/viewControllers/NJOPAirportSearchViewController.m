21b07e743a05aae77f47e7a36d67d4a53c909bf8
Angus Lo <angus.lo@havasww.com>
2015-01-12 18:04:56 -0500

    booking scene: added airport search view

---
+++ Tailwind/viewControllers/NJOPAirportSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  NJOPAirportSearchViewController.m
+//  Tailwind
+//
+//  Created by Angus.Lo on 1/12/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "NJOPAirportSearchViewController.h"
+
+@interface NJOPAirportSearchViewController ()
+
+@end
+
+@implementation NJOPAirportSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.searchInput becomeFirstResponder];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
