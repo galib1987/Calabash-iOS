8671675e4473868e21fe21d5358c02dcdeb1ee49
Rosa McGee <rosajmcgee@gmail.com>
2015-01-12 01:06:31 -0500

    Populated Account with sample cells until we can get Past Flights information.

--- Tailwind/viewControllers/NJOPAccountViewController.m
+++ Tailwind/viewControllers/NJOPAccountViewController.m
@@ -24,14 +24,60 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)loadDataSource {
+    
+    NSArray* sections = @[
+                          @{
+                              kSimpleDataSourceSectionCellsKey : @[
+                                      @{
+                                          kSimpleDataSourceCellIdentifierKey			: @"NJOPFlightTableCell",
+                                          kSimpleDataSourceCellKeypaths					: @{
+                                                  @"monthLabel.text" : @"JUN",
+                                                  @"dateLabel.text" : @"31", // placeholder value
+                                                  @"weekdayLabel.text" : @"Tuesday",
+                                                  @"toFBOLocationLabel.text" : @"Yaoundé",
+                                                  @"fromFBOLocationLabel.text" : @"Boca Raton",
+                                                  @"timeDurationLabel.text" : @"12:00PM - 2:45AM",
+                                                  }
+                                          },
+                                      @{
+                                          kSimpleDataSourceCellIdentifierKey			: @"NJOPFlightTableCell",
+                                          kSimpleDataSourceCellKeypaths					: @{
+                                                  @"monthLabel.text" : @"JUN",
+                                                  @"dateLabel.text" : @"31", // placeholder value
+                                                  @"weekdayLabel.text" : @"Tuesday",
+                                                  @"toFBOLocationLabel.text" : @"Sao Paolo",
+                                                  @"fromFBOLocationLabel.text" : @"Vancouver",
+                                                  @"timeDurationLabel.text" : @"12:00PM - 2:45AM",
+                                                  }
+                                          },
+                                      @{
+                                          kSimpleDataSourceCellIdentifierKey			: @"NJOPFlightTableCell",
+                                          kSimpleDataSourceCellKeypaths					: @{
+                                                  @"monthLabel.text" : @"JUN",
+                                                  @"dateLabel.text" : @"31", // placeholder value
+                                                  @"weekdayLabel.text" : @"Tuesday",
+                                                  @"toFBOLocationLabel.text" : @"Seoul",
+                                                  @"fromFBOLocationLabel.text" : @"Tokyo",
+                                                  @"timeDurationLabel.text" : @"12:00PM - 2:45AM",
+                                                  }
+                                          },
+                                      ]
+                              }
+                          ];
+    
+    self.dataSource = [SimpleDataSource dataSourceWithSections:sections];
+    self.dataSource.title = [@"Book a Flight" uppercaseString];
 }
-*/
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
