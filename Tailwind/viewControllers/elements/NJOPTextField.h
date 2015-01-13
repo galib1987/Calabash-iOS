3245091a955357f80d7928a519024d75dab9678e
Angus Lo <angus.lo@havasww.com>
2015-01-13 17:50:22 -0500

    booking scene: added custom text field (with padding and drop down arrow); put in correct placeholder text

---
+++ Tailwind/viewControllers/elements/NJOPTextField.h
@@ -0,0 +1,14 @@
+//
+//  NJOPTextField.h
+//  Tailwind
+//
+//  Created by Angus.Lo on 1/13/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NJOPTextField : UITextField
+@property (nonatomic) BOOL hasDropDown;
+
+@end
