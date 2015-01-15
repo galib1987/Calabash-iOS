0b4f0cc9b37a06e8efb8ab05a96adf5cdb027301
Stephen Cohrs <stephen.cohrs@havasww.com>
2015-01-15 11:37:04 -0500

    point to correct version of calendar and add custom classes for styling

---
+++ Tailwind/viewControllers/NJOPDatePickerDayCell.h
@@ -0,0 +1,13 @@
+//
+//  NJOPDatePickerDayCell.h
+//  Tailwind
+//
+//  Created by Stephen.Cohrs on 1/14/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "RSDFDatePickerDayCell.h"
+
+@interface NJOPDatePickerDayCell : RSDFDatePickerDayCell
+
+@end
