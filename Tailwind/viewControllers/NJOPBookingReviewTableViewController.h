6b29ab6ace3aaf492434602801fbc0f857852489
Angus Lo <angus.lo@havasww.com>
2015-01-16 16:35:28 -0500

    booking scene: start of review section

---
+++ Tailwind/viewControllers/NJOPBookingReviewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPBookingReviewTableViewController.h
+//  Tailwind
+//
+//  Created by Angus.Lo on 1/16/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "SimpleDataSourceTableViewController.h"
+
+@interface NJOPBookingReviewTableViewController : SimpleDataSourceTableViewController
+
+@end
---
+++ Tailwind/viewControllers/elements/NJOPPastFlightTableCell.m
@@ -0,0 +1,23 @@
+//
+//  NJOPPastFlightTableCell.m
+//  Tailwind
+//
+//  Created by Angus.Lo on 1/16/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "NJOPPastFlightTableCell.h"
+
+@implementation NJOPPastFlightTableCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
