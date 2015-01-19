18258b6b17815258c23a69e1ccbaf38171e1264f
Rosa McGee <rosajmcgee@gmail.com>
2015-01-19 09:41:43 -0500

    Connected storyboard to NJOPPlaneViewController.

---
+++ Tailwind/viewControllers/NJOPPlaneViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPPlaneViewController.h
+//  Tailwind
+//
+//  Created by netjets on 1/19/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "SimpleDataSourceTableViewController.h"
+
+@interface NJOPPlaneViewController : SimpleDataSourceTableViewController
+
+@end
