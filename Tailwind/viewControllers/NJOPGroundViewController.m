4786e0881145ad45fa2fb0f876a4713afcc8fb72
Rosa McGee <rosajmcgee@gmail.com>
2015-01-20 16:19:41 -0500

    Refactored Ground Transportation to better format data.

--- Tailwind/viewControllers/NJOPGroundViewController.m
+++ Tailwind/viewControllers/NJOPGroundViewController.m
@@ -31,40 +31,93 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)loadDataSource {
+- (NSString *)formatDateString:(NSDate *)date forDate:(BOOL)forDate {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     
+    if (forDate == YES) {
+        [formatter setDateFormat:@"EEEE MMM dd, YYYY"];
+    } else {
+        [formatter setDateFormat:@"hh:mm a"];
+    }
+    
+    return [formatter stringFromDate:date];
+    
+}
+
+- (NSString *)parseAndFormatRoute:(NSArray *)routeDescriptions {
+    
+    NSMutableString *routeString = [[NSMutableString alloc] init];
+    
+    for (NSString *desc in routeDescriptions) {
+        [routeString appendString:[NSString stringWithFormat:@"%@ \n", desc]];
+    }
+    
+    return routeString;
+}
+
+- (NSString *)parseAndFormatPassengers:(NSArray *)passengerRepresentations {
+    NSMutableString *manifestString = [[NSMutableString alloc] init];
+    
+    for (NSDictionary *passengerRepresentation in passengerRepresentations) {
+        NSString *currentPassengerName = passengerRepresentation[@"passengerName"];
+        [manifestString appendString:[NSString stringWithFormat:@"%@ \n", currentPassengerName]];
+    }
+    
+    NSLog(@"%@", manifestString);
+    return manifestString;
+}
+
+- (NSDictionary *)createDepartureCellFromReservation:(NSDictionary *)departureGroundOrder {
+    
+    NSDictionary *cellRepresentation = @{
+                                         kSimpleDataSourceCellIdentifierKey			: @"NJOPGroundCellDeparture",
+                                         kSimpleDataSourceCellKeypaths					: @{
+                                                 @"topLabel.text" : @"DEPARTURE",
+                                                 @"groundTypeLabel.text" : [NSString stringWithFormat:@"Vehicle #1 - %@", departureGroundOrder[@"groundType"]],
+                                                 @"autoSizeLabel.text" : departureGroundOrder[@"autoSize"],
+                                                 @"carServiceLabel.text" : departureGroundOrder[@"vendorName"],
+                                                 @"pickupTimeLabel.text" : [self formatDateString:_reservation.departureDate forDate:NO],
+                                                 @"pickupDateLabel.text" : [self formatDateString:_reservation.departureDate forDate:YES],
+                                                 @"routeDescLabel.text" : [self parseAndFormatRoute:departureGroundOrder[@"routeDescriptions"]],
+                                                 @"passengers.text" : [self parseAndFormatPassengers:self.reservation.passengers],
+                                                 }
+                                         };
+    
+    return cellRepresentation;
+    
+}
+
+
+- (NSDictionary *)createArrivalCellFromReservation:(NSDictionary *)arrivalGroundOrder {
+    
+    NSDictionary *cellRepresentation = @{
+                                         kSimpleDataSourceCellIdentifierKey			: @"NJOPGroundCellArrival",
+                                         kSimpleDataSourceCellKeypaths					: @{
+                                                 @"topLabel.text" : @"ARRIVAL",
+                                                 @"groundTypeLabel.text" : [NSString stringWithFormat:@"Vehicle #1 - %@", arrivalGroundOrder[@"groundType"]],
+                                                 @"autoSizeLabel.text" : arrivalGroundOrder[@"autoSize"],
+                                                 @"carServiceLabel.text" : arrivalGroundOrder[@"vendorName"],
+                                                 @"pickupTimeLabel.text" : [self formatDateString:_reservation.arrivalDate forDate:NO],
+                                                 @"pickupDateLabel.text" : [self formatDateString:_reservation.arrivalDate forDate:YES],
+                                                 @"routeDescLabel.text" : [self parseAndFormatRoute:arrivalGroundOrder[@"routeDescriptions"]],
+                                                 @"passengers.text" : [self parseAndFormatPassengers:self.reservation.passengers],
+                                                 }
+                                         };
+    
+    return cellRepresentation;
+    
+}
+
+- (void)loadDataSource {
+
     NSDictionary *departureGroundOrder = _reservation.groundOrders[0];
     NSDictionary *arrivalGroundOrder = _reservation.groundOrders[1];
     
     NSArray* sections = @[
                           @{
                               kSimpleDataSourceSectionCellsKey : @[
-                                      @{
-                                          kSimpleDataSourceCellIdentifierKey			: @"NJOPGroundCellDeparture",
-                                          kSimpleDataSourceCellKeypaths					: @{
-                                                  @"topLabel.text" : @"DEPARTURE",
-                                                  @"groundTypeLabel.text" : departureGroundOrder[@"groundType"],
-                                                  @"autoSizeLabel.text" : departureGroundOrder[@"autoSize"],
-                                                  @"carServiceLabel.text" : departureGroundOrder[@"vendorName"],
-                                                  @"pickupTimeLabel.text" : _reservation.departureTime,
-                                                  @"pickupDateLabel.text" : _reservation.departureDateString,
-                                                  @"routeDescLabel.text" : departureGroundOrder[@"routeDescriptions"][0],
-                                                  @"passengers.text" : _reservation.passengers[0][@"passengerName"],
-                                                  }
-                                          },
-                                      @{
-                                          kSimpleDataSourceCellIdentifierKey			: @"NJOPGroundCellArrival",
-                                          kSimpleDataSourceCellKeypaths					: @{
-                                                  @"topLabel.text" : @"ARRIVAL",
-                                                  @"groundTypeLabel.text" : arrivalGroundOrder[@"groundType"],
-                                                  @"autoSizeLabel.text" : arrivalGroundOrder[@"autoSize"],
-                                                  @"carServiceLabel.text" : arrivalGroundOrder[@"vendorName"],
-                                                  @"pickupTimeLabel.text" : _reservation.arrivalTime,
-                                                  @"pickupDateLabel.text" : _reservation.arrivalDateString,
-                                                  @"routeDescLabel.text" : arrivalGroundOrder[@"routeDescriptions"][0],
-                                                  @"passengers.text" : _reservation.passengers[0][@"passengerName"],
-                                                  }
-                                          }
+                                      [self createArrivalCellFromReservation:departureGroundOrder],
+                                      [self createDepartureCellFromReservation:arrivalGroundOrder]
                                       ]
                               }
                           ];
