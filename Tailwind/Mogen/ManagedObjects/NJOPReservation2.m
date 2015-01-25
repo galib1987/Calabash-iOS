51e439f4524e6a394b1ec88e6793d680590a74ff
Rosa McGee <rosajmcgee@gmail.com>
2015-01-25 01:34:19 -0500

    Added additional calls to the new HTTP client. Adding some basic elements to Core Data. Testing.

--- Tailwind/Mogen/ManagedObjects/NJOPReservation2.m
+++ Tailwind/Mogen/ManagedObjects/NJOPReservation2.m
@@ -1,4 +1,5 @@
 #import "NJOPReservation2.h"
+#import "NSDictionary+ObjectForKeyOrNil.h"
 
 @interface NJOPReservation2 ()
 
@@ -9,5 +10,32 @@
 @implementation NJOPReservation2
 
 // Custom logic goes here.
+
+//+ (NJOPReservation2 *)reservationWithReservationId:(NSInteger)reservationId usingManagedObjectContext:(NSManagedObjectContext *)moc {
+//    
+//    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[NJOPReservation2 entityName]];
+//    [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"reservationId = %d", reservationId]];
+//    [fetchRequest setFetchLimit:1];
+//    
+//    NSError *error = nil;
+//    
+//    NSArray *results = [moc executeFetchRequest:fetchRequest error:&error];
+//    
+//    
+//    if (error) {
+//        NSLog(@"ERROR: %@ %@", [error localizedDescription], [error userInfo]);
+//        exit(1);
+//    }
+//    
+//    if ([results count] == 0) {
+//        return nil;
+//    }
+//    
+//    return [results objectAtIndex:0];
+//}
+//
+//- (void)updateAttributes:(NSDictionary *)attributes {
+//    self.reservationID = [attributes objectForKeyOrNil:attributes];
+//}
 
 @end
