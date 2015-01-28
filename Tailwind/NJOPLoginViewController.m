022ed920835717d8639fa1e61901c6b6fe710f2e
Amin Heidari <Amin@Amins-MacBook-Pro.local>
2015-01-27 22:05:51 -0500

    reverted back the tmp code

--- Tailwind/NJOPLoginViewController.m
+++ Tailwind/NJOPLoginViewController.m
@@ -55,10 +55,6 @@
     tap.cancelsTouchesInView = NO;
     [self.view addGestureRecognizer:tap];
 
-	// remove it
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[self signInAction:nil];
-	});
 }
 
 -(BOOL)shouldAutorotate {
--- Tailwind/viewControllers/menu/NJOPMenuViewController.m
+++ Tailwind/viewControllers/menu/NJOPMenuViewController.m
@@ -21,10 +21,6 @@
     self.OSViewController = nil;
     [self setMenuSizesAndPositions];
 	
-	// remove it
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[self hamburgerPushed:nil];
-	});
 }
 
 - (void)didReceiveMemoryWarning {
