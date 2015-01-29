dc563bc666b80abb044b734e05b1cb37f92ad11e
Rosa McGee <rosajmcgee@gmail.com>
2015-01-29 14:39:58 -0500

    Converting Flights Detail into a table within a scrollview.

---
+++ Tailwind/viewControllers/NJOPDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJOPDetailViewController.h
+//  Tailwind
+//
+//  Created by netjets on 1/29/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NJOPDetailViewController : UIViewController
+
+@end
