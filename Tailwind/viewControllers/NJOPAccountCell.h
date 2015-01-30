34f6a5ba9419fe3d7fc3d588ea94bc3ea484e0bb
Rosa McGee <rosajmcgee@gmail.com>
2015-01-30 03:43:46 -0500

    Added cells and Account cards to Account screen.

---
+++ Tailwind/viewControllers/NJOPAccountCell.h
@@ -0,0 +1,16 @@
+//
+//  NJOPAccountCell.h
+//  Tailwind
+//
+//  Created by netjets on 1/30/15.
+//  Copyright (c) 2015 NetJets. All rights reserved.
+//
+
+#import "NJOPTableViewCell.h"
+
+@interface NJOPAccountCell : NJOPTableViewCell
+
+@property (nonatomic) IBOutlet UILabel *accountName;
+@property (nonatomic) IBOutlet UILabel *principalName;
+
+@end
